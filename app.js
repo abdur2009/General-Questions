fe653d5c9a2037a799a32020ad91952927f26e71
abdur2009 <abdurrahmanbatavia@gmail.com>
2025-01-20 22:53:42 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 function open1(){
     console.log("hello world")
 //     document.querySelector('.ansSec').innerHTML=""   
-    document.querySelector('.ansSec').classList.toggle("ansSec1go")
+    document.querySelector('.ansSec1go').classList.toggle("ansSec1go")
 }
 function open2(){
     console.log("hello world")
 //     document.querySelector('.ansSec').innerHTML=""   
-    document.querySelector('.ansSec2').classList.toggle("ansSec2go")
+    document.querySelector('.ansSec2go').classList.toggle("ansSec2go")
 }
 function open3(){
     console.log("hello world")
 //     document.querySelector('.ansSec').innerHTML=""   
-    document.querySelector('.ansSec3').classList.toggle("ansSec3go")
+    document.querySelector('.ansSec3go').classList.toggle("ansSec3go")
 }
